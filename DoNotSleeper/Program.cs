2a367a923e636398046b6707ea8fd0d9e3cfc13e
roman <r.sorokin@tvbet.tv>
2023-06-11 13:06:37 +0300

    Initial commit

---
+++ DoNotSleeper/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing;
+
+Console.WriteLine("Awake your computer! Press any key to stop.");
+var isWorking = true;
+while (isWorking)
+{
+    var mousePosition = Cursor.Position;
+    Cursor.Position = new Point(mousePosition.X - 10, mousePosition.Y - 10);
+    for (var i = 0; i < 60 && isWorking; i++)
+    {
+        await Task.Delay(1000);
+        if(Console.KeyAvailable)
+        {
+            isWorking = false;
+        }
+    }
+   
+}
